c471c46ff8684d1aa8cb76fcace577d0ce738d37
Bazmahou <bouissihamza6@gmail.com>
2021-12-03 06:17:19 +0100

    add

---
+++ spring-boot-docker/src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,11 @@
+package com.example.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	
+
+}
